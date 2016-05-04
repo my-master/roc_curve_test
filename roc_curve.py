64408e998e4b3fb76e0b3120bdda2297cc28b944
my-master <ol.gure@gmail.com>
2016-05-05 04:30:40 +0600

    minor fixes

--- roc_curve.py
+++ roc_curve.py
@@ -6,23 +6,23 @@ def roc_curve(y_true, y_prob):
     """
     :param y_true: numpy array of true Y values
     :param y_prob: numpy array of predicted Y values or probabilities
-    :yield: fpr, tpr, threshold tuple
+    :yield: fpr, tpr, threshold
     """
     all_pos = np.count_nonzero(y_true)
     all_neg = y_true.size - all_pos
     p = np.argsort(-y_prob)
     y_prob_sorted = y_prob[p]
     y_true_sorted = y_true[p]
-    threshes = y_prob_sorted
+    threshs = y_prob_sorted
     thresh_max = np.max(y_prob) + 1
     fp = 0
     tp = 0
-    for i in range(threshes.size):
-        if threshes[i] != thresh_max:
-            thresh_max = threshes[i]
+    for i in range(threshs.size):
+        if threshs[i] != thresh_max:
+            thresh_max = threshs[i]
             fpr = fp / all_neg
             tpr = tp / all_pos
-            yield fpr, tpr, threshes[i]
+            yield fpr, tpr, threshs[i]
         if y_true_sorted[i] == 1:
             tp +=1
         else:
@@ -31,21 +31,21 @@ def roc_curve(y_true, y_prob):
 
 def plot_roc_curve(y_true, y_probas):
     plt.figure(figsize=(16, 10))
-    for clf_name, predicts in y_probas.items():
-        rates = [(fpr, tpr) for fpr, tpr, _ in roc_curve(y_true, predicts)]
+    for clf_name, probas in y_probas.items():
+        rates = [(fpr, tpr) for fpr, tpr, _ in roc_curve(y_true, probas)]
         fpr = [r[0] for r in rates]
         tpr = [r[1] for r in rates]
         roc_auc = auc(fpr, tpr)
         plt.plot(fpr, tpr, label='ROC curve for {} classifier (area = {:.2f})'.format(clf_name, roc_auc),
                  color=np.random.rand(3,1))
-        plt.plot([0, 1], [0, 1], 'k--')
-        plt.xlim([0.0, 1.0])
-        plt.ylim([0.0, 1.0])
-        plt.xlabel('False Positive Rate', fontsize='large')
-        plt.ylabel('True Positive Rate', fontsize='large')
-        plt.title('ROC Curves')
-        plt.legend(loc="lower right")
-        plt.grid()
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate', fontsize='large')
+    plt.ylabel('True Positive Rate', fontsize='large')
+    plt.title('ROC Curves')
+    plt.legend(loc="lower right")
+    plt.grid()
     plt.show()
 
 def test():
